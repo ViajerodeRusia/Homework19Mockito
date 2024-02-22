633acac01b4d781096758e350d944f4fa425fdfb
Denis Siliukov <abhem21rus@mail.ru>
2024-02-22 08:03:23 +0300

    Project initialization

---
+++ src/main/java/com/skypro/Homework19Mockito/Homework19MockitoApplication.java
@@ -0,0 +1,13 @@
+package com.skypro.Homework19Mockito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Homework19MockitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Homework19MockitoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/skypro/Homework19Mockito/Homework19MockitoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skypro.Homework19Mockito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Homework19MockitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
